fd000a051fdb96ac8214fa78681121cc61147a86
rupaponnuru <rupab8@gmail.com>
2015-05-03 16:04:19 +0100

    files updated

--- kirana-hub/client/client.js
+++ kirana-hub/client/client.js
@@ -1,32 +1,60 @@
-$(function (){
-   
-    $.ajax({
-      type: 'GET',
-      url: 'http://localhost:3000/collectionapi/user_details' ,
-      success: function(data) { 
-           console.log('success',data);
-           alert("success");
-          } ,
-      error: function() { alert("error"); }      
-  
-  }); 
-  
-  
-    /*   
-      var data1 = { "firstname":"ab","lastname":"ba","password":"ab","email":"asd@gmail.com" };
-       
-       $.ajax({
+/* $(function()
+{
+   var $fname = $('#fname');
+   var $lname = $('#lname');
+   var $pwd = $('#pwd');
+   var $email = $('#email');
+  $('#action').on('click',function(){
+
+     var user = {
+                  "Firstname" : $fname.val(),
+                  "Lastname" : $lname.val(),
+                  "Password" : $pwd.val(),
+                  "Email": $email.val()
+                };
+          $.ajax({
           type: 'POST',
-          url: 'http://localhost:3000/collectionapi/players' ,
-          data: JSON.stringify(data1),
-         // datatype: 'text',
+          url: 'http://localhost:3000/collectionapi/user_details' ,
+          data: JSON.stringify(user),
           success: function(newData){
-               alert("post success");
+               alert("success");
                },
           error: function(XMLHttpRequest, textStatus, errorThrown) {alert(XMLHttpRequest); alert(textStatus) ; console.log(errorThrown) ; },
          });
-   
-       */ 
-  
+  });
+}); */
 
-});
+function addUser(data)
+{
+
+    $.ajax({
+          type: 'POST',
+          url: 'http://localhost:3000/collectionapi/user_collection' ,
+          data: JSON.stringify(data),
+          success: function(newData){
+              // $('userstatus').text(newData.obj);
+              $('#userstatus').html(newData.obj);
+               },
+          error: function() { console.log("email already exists") ;
+                              $('#userstatus').html("email already exists");
+                            }    //function(XMLHttpRequest, textStatus, errorThrown)
+         });
+}
+
+function getUsers()
+{
+  $.ajax({
+         type : 'GET',
+         url  : 'http://localhost:3000/collectionapi/user_collection' ,
+         success: function(data) {
+         // console.log(data.objs);
+
+            $('#output').text(data.objs);
+
+
+
+         } ,
+      error: function() { alert("error"); }
+      });
+
+}
